c1701ed8f145ba67c06bf23c089f28de9c159823
Subhanshu Bansal <subhashu20135@iiitd.ac.in>
2023-03-26 14:08:17 +0530

    Added Standalone Server

---
+++ index.js
@@ -0,0 +1,19 @@
+const resolvers = require('./src/resolvers')
+const typedefs = require('./src/typedef')
+
+const { ApolloServer } = require('@apollo/server')
+const { startStandaloneServer } = require('@apollo/server/standalone')
+
+const server = new ApolloServer(
+    {
+        typedefs,
+        resolvers,
+    }
+)
+
+const apolloStandaloneServer = startStandaloneServer(server, {
+    listen: 6000,
+})
+apolloStandaloneServer.then(() => {
+    console.log('Server Started');
+})
